3b4fceca9528b66e76f844130866a764da623c84
Alex Lamb <alexlamb104@gmail.com>
2022-04-21 00:26:38 -0600

    Header styling complete, started on center

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <section className='header-section'>
+            <h3 className='home'>Home</h3>
+        </section>
+    </div>
+  )
+}
+
+export default Header
